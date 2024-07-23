e1487bb91548c6b3ed4e1f145d7ce75fe71eed95
Javier Rivera <solylunajavivi@gmail.com>
2024-07-23 12:38:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import JurisdictionSelector from './JurisdictionSelector.jsx';
+
+const App = () => {
+  return (
+      <div>
+        <main>
+            <JurisdictionSelector />
+        </main>
+      </div>
+  );
+};
+
+export default App
